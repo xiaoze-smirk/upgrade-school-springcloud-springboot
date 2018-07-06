e780a35863ca66d43dab00cad0fd9998a5c76aea
xiaoze <zitengliansan@qq.com>
2018-07-06 15:08:19 +0800

    增加HystrixDashboard监控界面

--- hystrix-dashboard/src/main/java/com/xiaoze/springcloud/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/xiaoze/springcloud/HystrixDashboardApplication.java
@@ -2,13 +2,13 @@ package com.xiaoze.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@EnableEurekaClient
+@EnableHystrixDashboard
 @SpringBootApplication
-public class GatewayServerApplication {
+public class HystrixDashboardApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayServerApplication.class, args);
+        SpringApplication.run(HystrixDashboardApplication.class, args);
     }
 }
